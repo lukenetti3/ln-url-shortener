4f44c0fd0dc81687b5ee555e05d3c3f138af4c96
Luke Netti <lukenetti@hey.com>
2021-01-15 15:18:36 -0500

    fix export modules

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 
-export default () => (
-  <h1 style={{ textAlign: "center" }}>Welcome to Luke's URL Shortener</h1>
-);
+export default function Header() {
+  return(
+    <h1 style={{ textAlign: "center" }}>Welcome to Luke's URL Shortener</h1>
+  )
+};
--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,8 +1,10 @@
 import React from "react";
 import logo from "../images/link-image.png";
 
-export default () => (
-  <div className="image-container">
-    <img src={logo} alt="link-shortener" className="img" />
-  </div>
-);
+export default function ImageContainer() {
+    return(
+    <div className="image-container">
+        <img src={logo} alt="link-shortener" className="img" />
+    </div>
+    )
+}
